42c48be23cfe965833ffe450777f5f682a1366b4
Thisan Mindiya Kulathilake <139038281+mindiya12@users.noreply.github.com>
2025-11-05 12:37:01 +0530

    test coverage

---
+++ low_coverage.py
@@ -0,0 +1,10 @@
+def function_without_tests():
+    """This function has no tests"""
+    x = 5
+    y = 10
+    z = x + y
+    return z
+
+def another_untested():
+    """Also no tests"""
+    return "untested"
