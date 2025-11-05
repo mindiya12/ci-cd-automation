6a763d67806819f77440d29e1cb5018fc42da75e
Thisan Mindiya Kulathilake <139038281+mindiya12@users.noreply.github.com>
2025-11-05 13:30:58 +0530

    test error fixed

--- test_cal.py
+++ test_cal.py
@@ -9,7 +9,7 @@ def test_add():
     assert add(2, 3) == 5
     assert add(-1, 1) == 0
     assert add(0, 0) == 0
-    assert add(5, 5) == 50
+    assert add(5, 5) == 10
 
 def test_subtract():
     """Test subtraction"""
